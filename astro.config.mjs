79ff8ba8a345b669cf31244f65da78753dacadf4
dave long <davelongdev@gmail.com>
2023-07-24 19:45:14 -0400

    remove preact integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://davelong.dev',
-  integrations: [preact(), sitemap()]
+  integrations: [sitemap()]
 });
